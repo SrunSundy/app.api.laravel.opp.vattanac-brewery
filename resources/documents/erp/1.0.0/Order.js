43cf4ab19394ac9770f74c462d2716b7279bfd6f
Sophanith <chhounsophanith@gmail.com>
2021-09-03 16:01:15 +0700

    [TASK] View order by outlet id api doc

--- resources/documents/erp/1.0.0/Order.js
+++ resources/documents/erp/1.0.0/Order.js
@@ -6,7 +6,7 @@
  *
  * @apiUse GetHeader
  * 
- * @apiParam {integer} outlet_id          The outlet id which get from OPP
+ * @apiParam {integer} outlet_id          The outlet id to list the order (Will get sample from OPP)
  * @apiUse DefaultListParameter
  *
  * @apiSuccessExample  Response (example):
@@ -31,6 +31,44 @@
             }
         ],
         "total": 20
+    }
+ }
+ *
+ * @apiUse MissingHeader
+ * @apiUse AuthorizationInvalid
+ * @apiUse HeaderInvalid
+ * @apiUse ErrorValidation
+ * @apiUse ServerServerError
+ */
+
+/**
+ * @api {Post} /v1/erp/order/{order_number}/outlet/{outlet_id} 2. View order by outlet
+ * @apiVersion 1.0.0
+ * @apiName View order by outlet
+ * @apiGroup Order
+ *
+ * @apiUse GetHeader
+ * 
+ * @apiParam {string}  order_number       The order number which get from order list
+ * @apiParam {integer} outlet_id          The outlet id which proceed the order
+ *
+ * @apiSuccessExample  Response (example):
+ HTTP/1.1 200 Success Request
+ {
+    "success": true,
+    "message": "",
+    "data": {
+        "id": 38,
+        "order_number": "SO20210800038",
+        "outlet_name": "Sokahkrom",
+        "agent_name": "Sophanith",
+        "coupon_code": "",
+        "sub_total": 20.6,
+        "percent_off": 0,
+        "amount_off": 0,
+        "total": 20.6,
+        "is_urgent": 0,
+        "order_status": "Pending"
     }
  }
  *
